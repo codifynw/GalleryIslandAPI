cbf452a568d73a26d28c61485e63711a37aea44f
Cody Wilson <cody@codyscapes.com>
2019-05-26 15:24:23 -0700

    secret key

--- app/app/old_settings.py
+++ app/app/old_settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a%h%$k0s^kem*3ff-!ez2v-z6#t()r4+2d!2&ta@$fwxx_nwjm'
+SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
