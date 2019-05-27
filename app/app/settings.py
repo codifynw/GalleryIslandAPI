8ce707a6360a7143a6ab93dde136b2cdef15705b
Cody Wilson <cody@codyscapes.com>
2019-05-26 23:40:59 -0700

    added leads w/ api

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'storages',
-    'demo'
+    'leads',
+    'demo',
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from demo.views import image_upload
 
 urlpatterns = [
-    path('', image_upload, name='upload'),
+    path('leads/', include('leads.urls')),
+    path('upload/', image_upload, name='upload'),
     path('admin/', admin.site.urls),
 ]
