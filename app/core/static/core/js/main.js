d74ad948e2f7fa33bbdcebeb114175b5b17f910a
Cody Wilson <cody@codyscapes.com>
2019-07-13 22:47:52 -0700

    menu, home

---
+++ app/core/static/core/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // $('.link-to-gallery').mouseenter(
+    //     function() {
+    //         $('.gallery-section').addClass('open');
+    //     }
+    // ).mouseleave(
+    //     function() {
+    //         $('.gallery-section').removeClass('open');
+    //     }
+    // )
+
+
+    $('.menu-item').click(function() {
+        $('.link-target').removeClass('open');
+        $('.menu-item').removeClass('active');
+        // $('.link-to-gallery').addClass('active');
+        $('.' + $(this).attr('data-target')).addClass('open');
+    })
+})
