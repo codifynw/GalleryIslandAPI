45227c64609b7f3eca9e464d20e1e3b0de77bf5b
Cody Wilson <cody@codyscapes.com>
2019-07-14 14:40:44 -0700

    animations and state

--- app/core/static/core/js/main.js
+++ app/core/static/core/js/main.js
@@ -17,4 +17,22 @@ $(document).ready(function(){
         $(this).addClass('active');
         $('.' + $(this).attr('data-target')).addClass('open');
     })
+
+    $('.gallery-link').click(function() {
+        $('body').addClass('menu-running');
+        setTimeout(function(){
+            $('.gallery-index').removeClass('open');
+        }, 300);
+        $('body').addClass('menu-running');
+        setTimeout(function(){
+            $('#fullwidth-video').fadeOut();
+            $('#gallery-container').show();
+        }, 800);
+        setTimeout(function(){
+            $('body').removeClass('menu-running');
+        }, 2000);
+
+        window.history.pushState({}, "Test Title", "/gallery/favorites");
+
+    })
 })
