f8e031d32f4e942eb4527659dcb7c531b84d2601
Cody Wilson <cody@codyscapes.com>
2019-08-03 12:19:10 -0700

    animating intro

--- app/core/static/core/js/animations.js
+++ app/core/static/core/js/animations.js
@@ -1,6 +1,13 @@
 /* global $, GI */
 import { TweenMax, Power2, TimelineLite } from 'gsap/TweenMax';
+import TweenLite from 'gsap/TweenLite';
 
 export function runLandingPageAnimation () {
-    console.log('run the landing animation');
+    const $text = $('.landing-text');
+    const $bg = $('.landing-animation');
+
+    var tl = new TimelineLite();
+    tl.to($text, 1, { opacity: 0, delay: 3.5 });
+    tl.to($bg, 2, { backgroundColor: 'rgba(0,0,0,0)' });
+    tl.to($bg, 0, { display: 'none' }, 5.5);
 }
